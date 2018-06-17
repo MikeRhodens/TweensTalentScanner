42876c080645403ed1f899acad1ae681e9bd694d
mikerhodens <mikeweer@hotmail.com>
2018-06-18 00:51:15 +0200

    tussentijde commit

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -16,12 +16,38 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+var access_token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vMTI3LjAuMC4xOjgwMDAvYXV0aC9sb2dpbiIsImlhdCI6MTUyOTI1ODM3MiwiZXhwIjoxNTI5Mjk0MzcyLCJuYmYiOjE1MjkyNTgzNzIsImp0aSI6ImtVTzZjT09HclRVbk5aSFQiLCJzdWIiOjQsInBydiI6ImY5MzA3ZWI1ZjI5YzcyYTkwZGJhYWVmMGUyNmYwMjYyZWRlODZmNTUifQ.hrH-bMHegvlyXVKqLRjN2ddCHW_ZrbB5WiEHtq1i-jQ";
 
 var app = {
     // Application Constructor
     initialize: function () {
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+
+
         document.getElementById('scan-button').addEventListener('click', function () {
+            const options = {
+                method: 'get',
+                headers: {Authorization: 'Bearer ' + access_token}
+            };
+
+            cordova.plugin.http.sendRequest('https://37abc11e.ngrok.io/students', options, function (response) {
+                // prints 200
+                var data = JSON.parse(response['data']);
+                var talents = document.getElementsByClassName('talent');
+                console.log(data);
+
+            }, function (response) {
+                // prints 403
+                console.log(response.status);
+
+                //prints Permission denied
+                console.log(response.error);
+            });
+
+
+
+
+
             cordova.plugins.barcodeScanner.scan(
                 function (result) {
                     if (!result.cancelled) {
@@ -29,36 +55,35 @@ var app = {
                         if (result.format == "QR_CODE") {
                             var value = result.text;
                             //na de scan
-                            document.getElementById('scan-container').style.display = "none";
-                            document.getElementById('result-container').style.display = "block";
-                            document.getElementById('backbutton').style.display = "block";
-
-                            const options = {
-                                method: 'get',
-                                data: {user: "ed.shenk@ma-web.nl", password: '1324'},
-                                headers: {Authorization: 'bearer:' + token}
-                            };
-
-                            var request = 'studens/';
-                            var id = value;
-
-                            var fullUrl = url + request + id;
-                            cordova.plugin.http.sendRequest(fullUrl, options, function (response) {
-                                // prints 200
-                                var data = JSON.parse(response['data']);
-                                var talents = document.getElementsByClassName('talent');
-                                talents[0].innerHTML = data['talenten'][0];
-                                talents[1].innerHTML = data['talenten'][1];
-                                talents[2].innerHTML = data['talenten'][2];
-
-                            }, function (response) {
-                                // prints 403
-                                console.log(response.status);
-
-                                //prints Permission denied
-                                console.log(response.error);
-                            });
-                            console.log(value);
+                            // document.getElementById('scan-container').style.display = "none";
+                            // document.getElementById('result-container').style.display = "block";
+                            // document.getElementById('backbutton').style.display = "block";
+                            //
+                            // const options = {
+                            //     method: 'get',
+                            //     headers: {Authorization: 'bearer ' + "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vMTI3LjAuMC4xOjgwMDAvYXV0aC9sb2dpbiIsImlhdCI6MTUyOTI1ODM3MiwiZXhwIjoxNTI5Mjk0MzcyLCJuYmYiOjE1MjkyNTgzNzIsImp0aSI6ImtVTzZjT09HclRVbk5aSFQiLCJzdWIiOjQsInBydiI6ImY5MzA3ZWI1ZjI5YzcyYTkwZGJhYWVmMGUyNmYwMjYyZWRlODZmNTUifQ.hrH-bMHegvlyXVKqLRjN2ddCHW_ZrbB5WiEHtq1i-jQ"}
+                            // };
+                            //
+                            // var request = 'students';
+                            //
+                            // var fullUrl = url + request;
+                            // cordova.plugin.http.sendRequest(fullUrl, options, function (response) {
+                            //     // prints 200
+                            //     var data = JSON.parse(response['data']);
+                            //     var talents = document.getElementsByClassName('talent');
+                            //     alert(data);
+                            //     talents[0].innerHTML = data['talenten'][0];
+                            //     talents[1].innerHTML = data['talenten'][1];
+                            //     talents[2].innerHTML = data['talenten'][2];
+                            //
+                            // }, function (response) {
+                            //     // prints 403
+                            //     alert(response.status);
+                            //
+                            //     //prints Permission denied
+                            //     alert(response.error);
+                            // });
+                            // alert(value);
                         } else {
                             alert("Sorry, only qr codes this time ;)");
                         }
@@ -84,24 +109,8 @@ var app = {
     // 'pause', 'resume', etc.
     onDeviceReady: function () {
         this.receivedEvent('deviceready');
-        var url = 'localhost:8000/';
-        var request = 'auth/login';
 
-        var fullUrl = url + request;
-        const options = {
-            method: 'get',
-            data: {user: "ed.shenk@ma-web.nl", password: '1324'}
-        };
-
-        cordova.plugin.http.sendRequest(fullUrl, options, function (response) {
-            window.token = response['access_token']
-        }, function (response) {
-            console.log(response.status);
-            console.log(response.error);
-        });
-
-
-    },
+          },
 
 
     // Update DOM on a Received Event
@@ -116,5 +125,27 @@ var app = {
         console.log('Received Event: ' + id);
     }
 };
+window.addEventListener('load',function(){
+    // var url = 'https://37abc11e.ngrok.io/';
+    // var request = 'auth/login';
+    // alert('test');
+    // var fullUrl = url + request;
+    // const options = {
+    //     method: 'post',
+    //     data: {
+    //         "user": "ed.schenk@ma-web.nl",
+    //         "password": "1234"
+    //     },
+    //     headers: {"Content-Type": "application/json"}
+    // };
+    //
+    // cordova.plugin.http.sendRequest(fullUrl, options, function (response) {
+    //     window.token = response['access_token'];
+    //     alert(window.token);
+    // }, function (response) {
+    //     console.log(response.status);
+    //     console.log(response.error);
+    // });
 
+});
 app.initialize();
